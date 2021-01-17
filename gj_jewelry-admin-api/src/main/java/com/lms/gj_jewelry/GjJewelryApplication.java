793ef172fcbe940f2e2dd94c77aac4b5f1fac3d6
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2021-01-18 01:46:36 +0900

    feat: Initialize basic admin-api module

---
+++ gj_jewelry-admin-api/src/main/java/com/lms/gj_jewelry/GjJewelryApplication.java
@@ -0,0 +1,13 @@
+package com.lms.gj_jewelry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjJewelryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjJewelryApplication.class, args);
+	}
+
+}
