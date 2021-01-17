fd0243b1a93a15cffbbe97b3a7700cafc470442b
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2021-01-18 02:04:09 +0900

    feat : Initialize common module

---
+++ gj_jewelry-common/src/main/java/com/lms/gj_jewelrycommon/GjJewelryCommonApplication.java
@@ -0,0 +1,13 @@
+package com.lms.gj_jewelrycommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GjJewelryCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GjJewelryCommonApplication.class, args);
+	}
+
+}
---
+++ gj_jewelry-common/src/test/java/com/lms/gj_jewelrycommon/GjJewelryCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.gj_jewelrycommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjJewelryCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
