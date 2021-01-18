3e0cc4be515fd412ebb171512c189bd52aa0b804
rhsehfm33 <44993366+rhsehfm33@users.noreply.github.com>
2021-01-18 23:00:43 +0900

    feat: Implement User entity

---
+++ gj_jewelry-common/src/main/java/com/lms/gj_jewelry/interfaces/User.java
@@ -0,0 +1,45 @@
+package com.lms.gj_jewelry.interfaces;
+
+import lombok.*;
+import org.hibernate.validator.constraints.UniqueElements;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@ToString
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty
+    private String account;
+
+    @NotEmpty
+    private String password;
+
+    @NotEmpty
+    private String status;
+
+    @Column(unique = true)
+    private String email;
+
+    @NotEmpty
+    private String phoneNumber;
+
+    @NotNull
+    private LocalDate createdAt;
+
+    private LocalDate updatedAt;
+
+    private LocalDate deletedAt;
+
+}
