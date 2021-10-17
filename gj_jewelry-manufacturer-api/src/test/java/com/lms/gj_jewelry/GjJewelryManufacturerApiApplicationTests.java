fe76e8d34e7d2d14cf73c93d7ce1f98c70a5d307
rhsehfm33 <rhsehfm33@gmail.com>
2021-10-17 23:17:58 +0900

    feat: Add manufacturer-api project

---
+++ gj_jewelry-manufacturer-api/src/test/java/com/lms/gj_jewelry/GjJewelryManufacturerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lms.gj_jewelry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GjJewelryManufacturerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
